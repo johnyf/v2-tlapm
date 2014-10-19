76e75dfb2f815b67b7aed765d34da14863e39e6c
libal <libal@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2014-10-19 18:35:38 +0000

    added configuration files
    git-svn-id: svn+ssh://inria/var/lib/svn/repository/tla/trunk@34555 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- src/sany.ml
+++ src/sany.ml
@@ -6,215 +6,302 @@
  *)
 
 open Commons
+open Xmlm
 
 type node =
-  | APSubstIn of ap_subst_in
-  | AssumeProve of assume_prove
-  | DefStep of def_step
-  | Expr of expr
-  | OpArg of op_arg
-  | Instance of instance
-  | NewSymb of new_symb
-  | Proof of proof
-  | FormalParam of formal_param
-  | Module of mule
-  | OpDecl of op_decl
-  | OpDef of op_def
-  | Assume of assume
-  | Theorem of theorem
-  | UseOrHide of use_or_hide
+  | N_ap_subst_in of ap_subst_in
+  | N_assume_prove of assume_prove
+  | N_def_step of def_step
+  | N_expr of expr
+  | N_op_arg of op_arg
+  | N_instance of instance
+  | N_new_symb of new_symb
+  | N_proof of proof
+  | N_formal_param of formal_param
+  | N_module of mule
+  | N_op_decl of op_decl
+  | N_op_def of op_def
+  | N_assume of assume
+  | N_theorem of theorem
+  | N_use_or_hide of use_or_hide
 
 and expr =
-  | At of at
-  | Decimal of decimal
-  | Label of label
-  | LetIn of let_in
-  | Numeral of numeral
-  | OpAppl of op_appl
-  | String of strng
-  | SubsrtIn of subst_in
+  | E_at of at
+  | E_decimal of decimal
+  | E_label of label
+  | E_let_in of let_in
+  | E_numeral of numeral
+  | E_op_appl of op_appl
+  | E_string of strng
+  | E_subst_in of subst_in
+
+and expr_or_op_arg =
+  | EO_expr of expr
+  | EO_op_arg of op_arg
+
+and expr_or_assume_prove =
+  | EA_expr of expr
+  | EA_assume_prove of assume_prove
+
+and  new_symb_or_expr_or_assume_prove =
+  | NEA_new_symb of new_symb
+  | NEA_expr of expr
+  | NEA_assume_prove of assume_prove
 
 and ap_subst_in = {
-  loc               : location;
-  lvl               : level;
+  location          : location;
+  level             : level;
   substs            : subst list;
   body              : node
 }
 
 and subst_in = {
-  loc               : location;
-  lvl               : level;
+  location          : location;
+  level             : level;
   substs            : subst list;
   body              : expr
 }
 
 and instance = {
-  loc               : location;
-  lvl               : level;
+  location          : location;
+  level             : level;
   name              : string;
   substs            : subst list;
   params            : formal_param list
 }
 
 and subst = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  op                : op_decl;
+  expr              : expr_or_op_arg
 }
 
 and assume = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  expr              : expr
 }
 
 and theorem = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  expr              : expr_or_assume_prove;
+  proof             : proof;
+  suffices          : bool
 }
 
 and assume_prove = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  assumes           : new_symb_or_expr_or_assume_prove list;
+  prove             : expr;
+  suffices          : bool;
+  boxed             : bool
 }
 
 and new_symb = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  op_decl           : op_decl;
+  set               : expr
 }
 
 and op_def =
-  | ModuleInstance of module_instance
-  | UserDefinedOp of user_defined_op
-  | BuiltinOp of builtin_op
+  | O_module_instance of module_instance
+  | O_user_defined_op of user_defined_op
+  | O_builtin_op of builtin_op
 
 and module_instance = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  name              : string
 }
 
 and user_defined_op = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  name              : string;
+  arity             : int;
+  body              : expr;
+  params            : (formal_param * bool (*is leibniz*)) list;
+  recursive         : bool
 }
 
 and builtin_op = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  name              : string;
+  arity             : int;
+  params            : (formal_param * bool (*is leibniz*)) list
 }
 
 and op_arg = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  name              : string;
+  arity             : int
 }
 
 and formal_param = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  name              : string;
+  arity             : int
 }
 
 and op_decl = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  name              : string;
+  arity             : int;
+  kind              : op_decl_kind
 }
 
-and proof = {
-  loc               : location;
-  lvl               : level;
-
-}
+and proof =
+  | P_omitted of omitted
+  | P_obvious of obvious
+  | P_by of by
+  | P_steps of steps
 
 and omitted = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
 }
 
 and obvious = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
 }
 
-and by = {
-  loc               : location;
-  lvl               : level;
+and expr_or_module_or_module_instance =
+  | EMM_expr of expr
+  | EMM_module of mule
+  | EMM_module_instance of module_instance
 
+and user_defined_op_or_module_instance_or_theorem_or_assume =
+  | UMTA_user_definewd_op of user_defined_op
+  | UMTA_module_instance of module_instance
+  | UMTA_theorem of theorem
+  | UMTA_assume of assume
+
+and by = {
+  location          : location;
+  level             : level;
+  facts             : expr_or_module_or_module_instance list;
+  defs              : user_defined_op_or_module_instance_or_theorem_or_assume list;
+  only              : bool
+}
+
+and steps = {
+  location          : location;
+  level             : level;
+  steps             : step list
 }
 
 and step =
-  | DefStep of def_step
-  | UseOrHide of use_or_hide
-  | Instance of instance
-  | Theorem of theorem
+  | S_def_step of def_step
+  | S_use_or_hide of use_or_hide
+  | S_instance of instance
+  | S_theorem of theorem
 
 and def_step = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  defs              : op_def list
 }
 
 and use_or_hide = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  facts             : expr_or_module_or_module_instance list;
+  defs              : user_defined_op_or_module_instance_or_theorem_or_assume list;
+  only              : bool;
+  hide              : bool
 }
 
 and at = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  except            : op_appl;
+  except_component  : op_appl
 }
 
 and decimal = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  mantissa          : int;
+  exponent          : int
 }
 
 and label = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  name              : string;
+  arity             : int;
+  body              : expr_or_assume_prove;
+  params            : formal_param list
 }
 
-and let_in = {
-  loc               : location;
-  lvl               : level;
+and op_def_or_theorem_or_assume =
+  | OTA_op_def of op_def
+  | OTA_theorem of theorem
+  | OTA_assume of assume
 
+and let_in = {
+  location          : location;
+  level             : level;
+  body              : expr;
+  op_defs           : op_def_or_theorem_or_assume list
 }
 
 and numeral = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  value             : int
 }
 
 and strng = {
-  loc               : location;
-  lvl               : level;
-
+  location          : location;
+  level             : level;
+  value             : string
 }
 
-and op_appl = {
-  loc               : location;
-  lvl               : level;
+and formal_param_or_module_or_op_decl_or_op_def_or_theorem_or_assume =
+  | FMOTA_formal_param of formal_param
+  | FMOTA_module of mule
+  | FMOTA_op_decl of op_decl
+  | FMOTA_op_def of op_def
+  | FMOTA_theorem of theorem
+  | FMOTA_assume of assume
 
+and op_appl = {
+  location          : location;
+  level             : level;
+  operator          : formal_param_or_module_or_op_decl_or_op_def_or_theorem_or_assume;
+  operands          : expr_or_op_arg list;
+  bound_symbols     : bound_symbol list
+}
+
+and bound_symbol =
+  | B_unbounded_bound_symbol of unbounded_bound_symbol
+  | B_bound_bound_symbol of bounded_bound_symbol
+
+and unbounded_bound_symbol = {
+  param             : formal_param;
+  tuple             : bool
+}
+
+and bounded_bound_symbol = {
+  params            : formal_param list;
+  tuple             : bool;
+  domain            : expr
 }
 
 (* modules *)
 and mule = {
   name              : string;
-  loc               : location;
+  location          : location;
   constants         : op_decl list;
   variables         : op_decl list;
   definitions       : op_def list ;
