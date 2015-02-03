3ffeef26406ab6e4e363b6143ed294f0b62e0511
mriener <mriener@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2015-02-03 18:20:05 +0000

    xml parser ctd: formal param, builtinop
    git-svn-id: svn+ssh://inria/var/lib/svn/repository/tla/trunk@34609 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- src/sany.ml
+++ src/sany.ml
@@ -313,6 +313,8 @@ and mule = {
 (* this map represents the context, mapping UIDs to formal_param_or_module_or_op_decl_or_op_def_or_theorem_or_assume *)
 module ContextMap =  Map.Make(struct type t = int let compare : int -> int -> int = compare end) (* Map.Make(Integer) *)
 
+
+
 (*
  * if context is given, then we check both tg and tg^"Ref"
  * and if there is Ref, we get the item from the context.
@@ -342,13 +344,15 @@ let get_child ?context:(con=None) i tg f =
 let open_tag i tg = match (input i) with
   | `El_start tag when (snd (fst tag) = tg) -> ()
   | `El_start tag -> failwith ("Illegal XML start tag: " ^ (snd(fst tag)) ^ ". expected: " ^ tg ^ ".")
+  | `El_end -> failwith ("Illegal tag encountered: expecting start tag for: " ^ tg ^
+				", but got end tag.")
   | _ -> failwith "Illegal XML element"
 
 let close_tag i tg = match (input i) with
   | `El_end -> () (* can we check that we expect the end to be </tg>? *)
   | `El_start d -> failwith ("Illegal tag encountered: expecting end tag for: " ^ tg ^
 				", but got start tag for: " ^ (snd(fst d)))
-  | _ -> failwith "Illegal XML element"
+  | _ -> failwith "Illegal XML element "
 
 (* expects a node named tg_par and returns the all the children with tag tg_chdren. 
    remark: tag is closed afterwards, cannot process any remaining children
@@ -379,35 +383,9 @@ let read_string i = match (input i) with
   | _ -> failwith "expected data element"
 
 
-
 let init_context_map ls = ContextMap.empty
-  
-let read_formal_param i =
-  open_tag i "FormalParamNode";
-  open_tag i "uniquename";
-  let un = get_data_in i "uniquename" read_string in
-  close_tag i "uniquename";
-  open_tag i "arity";
-  let ar = get_data_in i "arity" read_int in
-  close_tag i "FormalParamNode";
-  { arity = ar;
-    name = un;
-  }
 
-(* should be a map of entries *)
-let rec read_entry i =
-   let uid = get_data_in i "UID" read_int in
-   let name = match (peek i) with 
-     | `El_start ((_, name),_ ) -> name
-     | _ -> failwith "We expect symbol opening tag in an entry."
-   in 
-   let symbol = match name with 
-     | "FormalParamNode" -> read_formal_param i
-     | _ -> failwith ("Unhandled context node " ^ name)
-   in (uid, symbol)
-
-
-  
+(** Parses a location node, returning a record with line and column *)
 let read_location i =
   open_tag i "location";
   open_tag i "column";
@@ -423,6 +401,56 @@ let read_location i =
   { column = {rbegin = cb; rend = ce};
     line = {rbegin = lb; rend = le};
     filename = fname }
+
+  
+(** Parses the FormalParamNode within context/entry *)
+let read_formal_param i =
+  open_tag i "FormalParamNode";
+  let un = get_data_in i "uniquename" read_string in
+  let ar = get_data_in i "arity" read_int in
+  close_tag i "FormalParamNode";
+  { arity = ar;
+    name = un;
+  }
+
+(** Parses the BuiltinKind within context/entry *)
+let read_builtin_kind i =
+  open_tag i "BuiltInKind";
+  let loc = read_location i in
+  open_tag i "level";
+  let level = read_int i in
+  close_tag i "level";
+  let un = get_data_in i "uniquename" read_string in
+  let ar = get_data_in i "arity" read_int in
+  (* TODO: params *)
+  close_tag i "BuiltInKind";
+  { arity = ar;
+    name = un;
+  }
+
+let read_module_instance_kind i = assert false
+let read_userdefinedop_kind i  = assert false
+    
+(* should be a map of entries *)
+let rec read_entry i =
+   let _ = open_tag i "entry" in 
+   let uid = get_data_in i "UID" read_int in
+   let name = match (peek i) with 
+     | `El_start ((_, name),_ ) -> name
+     | _ -> failwith "We expect symbol opening tag in an entry."
+   in 
+   let symbol = Printf.printf "symbol\n"; match name with 
+     | "FormalParamNode" -> read_formal_param i
+     (* Operator definition nodes: ModuleInstanceKind, UserDefinedOpKind, BuiltinKind *)
+     | "UserDefinedOpKind" -> read_userdefinedop_kind i (* TODO *) 
+     | "ModuleInstanceKind" -> read_module_instance_kind i (* TODO *)
+     | "BuiltInKind" -> read_builtin_kind i
+     | _ -> failwith ("Unhandled context node " ^ name)
+   in let _ = close_tag i "entry";
+   in  (uid, symbol)
+
+
+  
 
 let read_name i = assert false
 let read_op_decl i = assert false
@@ -452,9 +480,7 @@ let read_module con i =
 
 let read_modules i =
   open_tag i "modules";
-  open_tag i "context"; 
   let con = (init_context_map (get_children_in i "context" "entry" read_entry) ) in 
-  close_tag i "context"; 
   let ret = get_children i "ModuleNode" (read_module (Some con)) in
   close_tag i "modules";
   ret
