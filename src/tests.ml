404740ac51450a1cd87b3373499f34c8a79ccdf9
mriener <mriener@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2015-02-04 13:46:02 +0000

    adding testing to oasis file.
    git-svn-id: svn+ssh://inria/var/lib/svn/repository/tla/trunk@34611 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ src/tests.ml
@@ -0,0 +1,11 @@
+open Printf
+
+open Kaputt.Abbreviations
+let t1 =
+  Test.make_simple_test
+    ~title:"first test"
+    (fun () -> Assert.equal_int 3 3)
+  
+let () = printf "Launching Tests\n"; Test.run_tests [t1]
+
+
