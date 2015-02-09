2577e07835f852205d873e70a1aedb692aadf749
mriener <mriener@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2015-02-09 09:49:41 +0000

    test setup finally works
    git-svn-id: svn+ssh://inria/var/lib/svn/repository/tla/trunk@34615 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- src/sany.ml
+++ src/sany.ml
@@ -2,6 +2,8 @@
  *
  * SANY expressions
  * This file should be broken into xml utilities, sany ds and sany export
+ * Remark: the parser does not chek for additional data between nodes, e.g. 
+ * adding newlines will result in parsing failures.
  *
  * Author: TL
  *)
@@ -344,6 +346,11 @@ and mule_ = {
 module ContextMap =  Map.Make(struct type t = int let compare : int -> int -> int = compare end) (* Map.Make(Integer) *)
 
 
+let formatSignal (i : Xmlm.signal) = match i with
+  | `El_start tg -> "<" ^ (snd (fst tg)) ^ ">"
+  | `El_end -> "</>"
+  | `Dtd t -> "dtd"
+  | `Data str -> "Data:" ^ str 
 
 (*
  * if context is given, then we check both tg and tg^"Ref"
@@ -363,7 +370,7 @@ let rec get_children ?context:(con=None) i tg f =
       child :: (get_children ~context:con i tg f)
   | `El_start tag (* otherwise *) -> []
   | `El_end -> []
-  | _ -> failwith "Illegal XML element"
+  | _ -> failwith ("Illegal XML element, expecting child nodes: " ^ (formatSignal (peek i)))
 
     
 let get_child ?context:(con=None) i tg f =
@@ -371,18 +378,18 @@ let get_child ?context:(con=None) i tg f =
   assert (List.length chldn = 1);
   List.hd chldn
 
-let open_tag i tg = match (input i) with
+let open_tag i tg = let signal = (input i) in match signal  with
   | `El_start tag when (snd (fst tag) = tg) -> ()
   | `El_start tag -> failwith ("Illegal XML start tag: " ^ (snd(fst tag)) ^ ". expected: " ^ tg ^ ".")
   | `El_end -> failwith ("Illegal tag encountered: expecting start tag for: " ^ tg ^
 				", but got end tag.")
-  | _ -> failwith "Illegal XML element"
+  | _ -> failwith ("Illegal XML element, expecting open tag: " ^ (formatSignal signal))
 
 let close_tag i tg = match (input i) with
   | `El_end -> () (* can we check that we expect the end to be </tg>? *)
   | `El_start d -> failwith ("Illegal tag encountered: expecting end tag for: " ^ tg ^
 				", but got start tag for: " ^ (snd(fst d)))
-  | _ -> failwith "Illegal XML element "
+  | _ -> failwith ("Illegal XML element, expecting close tag: " ^ (formatSignal (peek i)))
 
 (* expects a node named tg_par and returns the all the children with tag tg_chdren. 
    remark: tag is closed afterwards, cannot process any remaining children
--- src/tests.ml
+++ src/tests.ml
@@ -1,11 +1,14 @@
 open Printf
 
 open Kaputt.Abbreviations
+
 let t1 =
   Test.make_simple_test
     ~title:"first test"
     (fun () -> Assert.equal_int 3 3)
   
-let () = printf "Launching Tests\n"; Test.run_tests [t1]
+let () =
+  printf "Launching Tests\n";
+  Test.run_tests ( Test_sany.get_tests ) 
 
 
