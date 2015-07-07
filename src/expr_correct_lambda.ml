a404aac24a141e824414329c4620a5c03e41c126
Martin <marty@logic.at>
2015-07-07 16:08:25 +0200

    forgot to commit 2 files

---
+++ src/expr_correct_lambda.ml
@@ -0,0 +1,37 @@
+open Commons
+open Expr_ds
+open Expr_map
+open Any_expr
+
+class ['a] correct_lambda = object(self)
+inherit ['a] expr_map as super
+
+method expr acc = function
+  | other -> super#expr acc other
+
+
+
+(* *)
+method expr_or_op_arg acc eo = match eo with
+| EO_op_arg ({ location; level; argument  } ) ->
+   ( match argument with
+     | FMOTA_op_def
+       (OPDef
+        (O_user_defined_op
+         (UOP {location; level; name="LAMBDA"; arity; body; params;})) ) ->
+        (* TODO: exclude real user definitions called LAMBDA *)
+        let acc1 = self#level acc level in
+        let acc2 = self#expr acc1 body in
+        let fparams, leibniz = List.split params in
+        let ie = self#get_id_extractor in
+        let rfparams, acc3 = unpack_fold ie#formal_param
+                                         self#formal_param acc2 fparams in
+        let params = List.combine rfparams leibniz in
+        let r = E_lambda ({level; arity; body; params}) in
+        set_anyexpr acc (Any_expr_or_op_arg (EO_expr r))
+     (* i've seen only inline UOP lambdas so far, but there might be references *)
+     (* TODO: check references*)
+     | _ -> super#expr_or_op_arg acc eo
+   )
+| _ -> super#expr_or_op_arg acc eo
+end
---
+++ src/test_correct_lambda.ml
@@ -0,0 +1,39 @@
+open Kaputt.Abbreviations
+open Any_expr
+open Expr_ds
+open Expr_correct_lambda
+open Expr_formatter
+open Util
+open Test_common
+
+
+let test_expr_map record () =
+  Printf.printf "%s\n" record.filename;
+  let mapper = new correct_lambda in
+  let context = match record.expr_context with
+  | Some x -> x
+  | None -> failwith ("No expression content for " ^ record.filename ^ "!")
+  in
+  let me = mapper#get_macc_extractor in
+  let mapped_context =
+    me#context (mapper#context (Nothing, []) context) in
+  let result = context = mapped_context in
+  Printf.printf "%s\n%b\n" record.filename result;
+  expr_formatter#context (Format.std_formatter, mapped_context, true, Module, 0)
+                         mapped_context; (* *)
+  (*  Assert.equal context mapped_context *)
+  ()
+
+
+let test_map record =
+  Test.make_assert_test
+    ~title: ("correcting lambda on expr ds trees " ^ record.filename)
+    (fun () -> ())
+    (fun () ->
+     Assert.no_raise ~msg:"Unexpected exception raised."
+                     (fun () -> exhandler ( test_expr_map record )  )
+    )
+    (fun () -> ()  )
+
+
+let get_tests records = List.map test_map records
