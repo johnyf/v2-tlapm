26d858f391c3c569325770f35aafac9bd1f479e5
Martin <marty@logic.at>
2016-06-02 20:02:09 +0200

    added test for issue 2, not fixed yet

---
+++ src/test_issue2.ml
@@ -0,0 +1,40 @@
+open Kaputt
+open Kaputt.Abbreviations
+open Any_expr
+open Expr_ds
+open Expr_map
+open Expr_formatter
+open Obligation
+open Obligation_formatter
+open Format
+open Util
+open Test_common
+
+
+
+let test_map record =
+  Test.make_assert_test
+    ~title: ("testing bug of issue 2:" ^ record.filename)
+    (fun () -> record )
+    (function
+        { obligations; _ } ->
+        Assert.is_false (obligations = []);
+        List.map (function
+                   | { goal = { assumes = []; _}; _ } ->
+                      ()
+                   | obl ->
+                      let msg = sprintf
+                                  "Expected no assumptions in goal of obligation %a"
+                                  fmt_obligation obl
+                      in
+                      Assert.fail_msg msg
+                 ) obligations;
+        ()
+    )
+    (fun () -> ()  )
+
+
+let get_tests records =
+  let re = Str.regexp ".*obligation_bug1.xml$" in
+  let record = List.find (fun x -> Str.string_match re x.filename 0) records in
+  [test_map record]
--- src/tests.ml
+++ src/tests.ml
@@ -46,6 +46,7 @@ let files flt =
            "OneBit";
            (* contains duplicates of multiple modules, takes long to load *)
            (*"pharos";  *)
+           (* "obligation_bug1" TODO: fix this bug! *)
            ])
 
 let () =
@@ -65,7 +66,8 @@ let () =
         Test_correct_lambda.get_tests without_broken;
         Test_parse_theorems.get_tests without_broken;
         Test_extract_obligations.get_tests without_broken;
-        Test_formatter.get_tests without_broken (* *)
+        Test_formatter.get_tests without_broken (* *);
+        Test_issue2.get_tests without_broken;
       ] in
   match check_xmloutput with
   | true  ->
